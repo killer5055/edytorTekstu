d68cd4f0e688129037b19b751cd9e909db7410ff
killer5055 <32611207+killer5055@users.noreply.github.com>
2017-10-08 11:44:24 +0200

    projekt
    comit podstawowego projektu

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.rtbDocument = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbDocument
+            // 
+            this.rtbDocument.Location = new System.Drawing.Point(12, 27);
+            this.rtbDocument.Name = "rtbDocument";
+            this.rtbDocument.Size = new System.Drawing.Size(984, 445);
+            this.rtbDocument.TabIndex = 1;
+            this.rtbDocument.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 492);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Zapisz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(94, 491);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Otwórz";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 527);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rtbDocument);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.RichTextBox rtbDocument;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "txt files (*.txt)|*.txt";           
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    File.WriteAllText(sfd.FileName, rtbDocument.Text);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not save file at disk. Original error: " + ex.Message);
+                }
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "txt files (*.txt)|*.txt";
+            //ofd.OpenFile();
+            String calosc = String.Empty;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    Stream myStream;
+                    if ((myStream = ofd.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            // Insert code to read the stream here.
+                            byte[] bytes = new byte[myStream.Length];
+                            int numBytesToRead = (int)myStream.Length;
+                            int numBytesRead = 0;
+                            StreamReader c = new StreamReader(myStream);
+                            while (numBytesToRead > 0)
+                            {                           
+
+                               calosc = c.ReadToEnd();
+                                // Read may return anything from 0 to numBytesToRead.
+                                int n = myStream.Read(bytes, numBytesRead, numBytesToRead);
+                                // Break when the end of the file is reached.
+                                if (n == 0)
+                                    break;
+
+                                numBytesRead += n;
+                                numBytesToRead -= n;
+                            }
+                            numBytesToRead = bytes.Length;                    
+                        }
+                    }
+                    rtbDocument.Text = calosc;
+                   
+                }         
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+
+        }
+    }
+}
+
+
+/*
+ DO ZROBIENIA ZA TYDZIEN
+ 1)  //rtbDocument.Find() - WYSZUKIWANIE W TEKSCIE JAKIEGOS WYRAZU --> TEXTBOX PO KLIKNIECIU NA ZNAJDZ - WPISUJEMY TEKST 
+ 2) ZMIANA CZCIONKI I ROZMAIRU ITP  
+     */
